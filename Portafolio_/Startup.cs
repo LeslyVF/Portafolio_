ef37d6e90addf2d41a0bfd6d228785587c3057b1
armentaspcs <armentaspcs@LESLY-VEGA10-F>
2022-11-25 10:12:43 -0800

    Agregar archivos de proyecto.

---
+++ Portafolio_/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Portafolio_.Startup))]
+namespace Portafolio_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
